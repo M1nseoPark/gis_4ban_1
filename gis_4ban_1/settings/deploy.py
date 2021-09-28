febb0373b46031acfe2e1eb2f8d2bff46dc0c184
MinseoPark <kjms0820@gmail.com>
2021-09-28 11:15:14 +0900

    9/28 4ban class commit 1

--- gis_4ban_1/settings/deploy.py
+++ gis_4ban_1/settings/deploy.py
@@ -18,7 +18,7 @@ while True:
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!s
+# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env_list['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -40,5 +40,3 @@ DATABASES = {
         'PORT': '3306',
     }
 }
-
-
